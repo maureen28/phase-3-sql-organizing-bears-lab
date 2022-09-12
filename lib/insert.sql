4afa075666c8501525d94812232e770862b496da
maureen <maureen.murimi@moringaschool.com>
2022-09-12 15:24:56 +0300

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) 
+VALUES 
+(1,'Mr. Chocolate', 20, 'M', 'blue', 'sanguine', True),
+(2,'Rowdy', 30, 'M', 'white', 'phlegmatic', False),
+(3,'Tabitha', 7, 'F', 'black', 'sanguine', True),
+(4,'Sergeant Brown', 25, 'M', 'brown', 'phlegmatic', False),
+(5,'Melissa', 12, 'F', 'beige', 'choleric', True),
+(6,'Grinch', 2, 'M', 'black', 'melancholic', True),
+(7,'Wendy', 5, 'F', 'brown', 'sanguine', True),
+(8, null,  13, 'F', 'dark brown', 'goofy', True);
